8d37e099a44f5998eb79c7a0fb744d35fa7cd2b8
Andy Leonard <26578720+ALeoLeonard@users.noreply.github.com>
2023-12-12 21:32:10 -0500

    Delete CSCIE49-final-project directory

--- CSCIE49-final-project/utilites/cognitoUtil.js
+++
@@ -1,5 +0,0 @@
-// Checks if the decoded JWT contains a group membership
-exports.isUserInGroup = (decodedToken, groupName) => {
-    const groups = decodedToken['cognito:groups'] || [];
-    return groups.includes(groupName);
-};
