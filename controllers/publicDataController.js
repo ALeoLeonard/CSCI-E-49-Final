828b7383b640722b902ec361fe1ea3821c590a56
Andy Leonard <26578720+ALeoLeonard@users.noreply.github.com>
2023-12-13 02:13:21 -0500

    Update publicDataController.js

--- controllers/publicDataController.js
+++ controllers/publicDataController.js
@@ -1,40 +1,50 @@
 const { getPublicData } = require('../services/queryService');
+const htmlHeadContent = require('../utilities/htmlHeadContent');
 
 exports.renderPublicDataPage = (req, res) => {
-    console.log('public data works')
     getPublicData((err, publicData) => {
         if (err) {
             return res.status(500).send('Error fetching public data');
         }
 
-        // Render the public data and the sign-in button as HTML
+        // Function to generate table rows for given data
+        const generateTableRows = (data) => {
+            return data.map(obj => `
+                <tr>
+                    ${Object.values(obj).map(val => `<td>${val || ''}</td>`).join('')}
+                </tr>
+            `).join('');
+        };
+
+        // Function to generate entire table for given data
+        const generateTable = (data) => {
+            if (data.length === 0) {
+                return '<p>No data available.</p>';
+            }
+
+            const tableHeaders = Object.keys(data[0]).map(key => `<th>${key}</th>`).join('');
+            const tableRows = generateTableRows(data);
+
+            return `
+                <table>
+                    <tr>${tableHeaders}</tr>
+                    ${tableRows}
+                </table>
+            `;
+        };
+
+        // Generate the HTML content for public data
         const htmlContent = `
-            <html>
-                <head>
-                    <title>Public Data</title>
-                    <style>
-                        /* Add styles for your button here */
-                        .signin-button {
-                            position: absolute;
-                            top: 10px;
-                            right: 10px;
-                            padding: 10px 20px;
-                            background-color: #007bff;
-                            color: white;
-                            text-decoration: none;
-                            border-radius: 5px;
-                        }
-                        .signin-button:hover {
-                            background-color: #0056b3;
-                        }
-                    </style>
-                </head>
+            <!DOCTYPE html>
+            <html lang="en">
+                ${htmlHeadContent}
                 <body>
-                    <a href="https://cscie49.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=10obrhnincsni076hep1ku17b3&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2F44.198.175.14%3A3000%2Foauth-callback" class="signin-button">Sign In</a>
                     <h1>Public Data</h1>
-                    <pre>${JSON.stringify(publicData, null, 2)}</pre>
+                    ${generateTable(publicData)}
+                    <a href="https://cscie49.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=10obrhnincsni076hep1ku17b3&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2F44.198.175.14%3A3000%2Foauth-callback" class="signin-button">Sign In</a>
                 </body>
             </html>`;
+
         res.send(htmlContent);
     });
 };
